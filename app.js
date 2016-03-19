f892d46ebe671454b262599f3b3e8951443e4a7c
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-19 16:37:06 +0000

    upvotes on comments now working

--- app.js
+++ app.js
@@ -40,9 +40,13 @@ function($scope, $stateParams, posts){
     $scope.post.comments.push({
       body: $scope.body,
       author: 'user',
-      upvotes: o
+      upvotes: 0
     });
     $scope.body = "";
+  };
+
+  $scope.incrementUpvotes = function(comment) {
+    comment.upvotes += 1;
   };
 }])
 
