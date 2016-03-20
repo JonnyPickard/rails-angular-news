58fd49e412d47e0a9272f8eae8887822e2404ce8
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-20 22:35:38 +0000

    added login and register inline templates

--- j-news/app/assets/javascripts/app.js
+++ j-news/app/assets/javascripts/app.js
@@ -147,4 +147,23 @@ function($scope, Auth){
   $scope.$on('devise:logout', function (e, user){
     $scope.user = {};
   });
+}])
+
+.controller('AuthCtrl', [
+'$scope',
+'$state',
+'Auth',
+function($scope, $state, Auth){
+
+  $scope.login = function() {
+    Auth.login($scope.user).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.register = function() {
+    Auth.register($scope.user).then(function(){
+      $state.go('home');
+    });
+  };
 }]);
