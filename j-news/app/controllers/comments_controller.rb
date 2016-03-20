e09d62a066ec2529bbb1a379bdc6bccca7b7063d
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-20 21:56:38 +0000

    comments working, removed respond with

--- j-news/app/controllers/comments_controller.rb
+++ j-news/app/controllers/comments_controller.rb
@@ -1,9 +1,8 @@
 class CommentsController < ApplicationController
-
   def create
     post = Post.find(params[:post_id])
     comment = post.comments.create(comment_params)
-    respond_with post, comment
+    render json: comment
   end
 
   def upvote
@@ -18,5 +17,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:body)
   end
-
 end
